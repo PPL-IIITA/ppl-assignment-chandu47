c0395dd5c4bd0d2c80ffcb58d4d2ee03582e8399
chandu47 <davchandu47@gmail.com>
2017-02-27 01:06:49 +0530

    Add files via upload

---
+++ ques2/girl.py
@@ -0,0 +1,11 @@
+class Girl:
+	#Gal Classs
+
+	def __init__(self,gname,gattract,gintellij,gbudget,gtypes):
+		self.gname=gname
+		self.gattract=gattract
+		self.gintellij=gintellij
+		self.gbudget=gbudget
+		self.gtypes=gtypes
+		self.gstatus=False
+		self.bf=''
