fe23c8103852fda321cfa5e7180bdd125db8d431
chandu47 <davchandu47@gmail.com>
2017-04-10 02:18:06 +0530

    Add files via upload

---
+++ boy.py
@@ -0,0 +1,11 @@
+class Boy:
+	#Buoy Classs
+
+	def __init__(self,bname,battract,bintellij,bbudget,btypes,battractReq):
+		self.bname=bname
+		self.battract=battract
+		self.bintellij=bintellij
+		self.bbudget=bbudget
+		self.btypes=btypes
+		self.battractReq=battractReq
+		
